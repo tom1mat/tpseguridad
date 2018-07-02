c5f59d9f321f516ef7de01889a7239182100337d
FrancoPetrone <franco.petrone@davinci.edu.ar>
2018-07-02 14:58:00 -0300

    Merge branch 'master' of https://github.com/tom1mat/tpseguridad
    # Conflicts:
    #	resources/views/auth/login.blade.php
    #	resources/views/auth/register.blade.php

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,45 +1,95 @@
 @extends('template')
 
 @section('main')
-    <style>
-        textarea{
-            height: 110px !important;
-        }
-        .bg-gray-lighter > a{
-            color: #0b0b0b;
-            font-weight: bold;
-        }
-    </style>
     <div class="block block-bordered">
         <div class="block-header bg-gray-lighter" style="border-bottom: 0">
-            <a href="{{ route("patient.shifts.create") }}"><span>USUARIOS > NUEVO USUARIO</span></a>
+            <a href="{{ route("admin.index") }}"><span>USUARIOS > NUEVO</span></a>
         </div>
         <div class="block-content" style="padding: 0 35px">
-            <div class="row">
-                {{ Form::open(['route' => 'patient.shifts.create', 'method' => 'POST']) }}
-                <div class="col-sm-12">
-                    <h3 style="margin: 0 0  20px 15px;">Add new Dynamic data</h3>
-                </div>
-                <div class="col-sm-12">
-                    <div class="col-sm-4">
-                        <div class="form-group">
-                            <label>Type</label><br>
-                            {{--{!! Form::select('dynamicDataTypeId', $dynamicDataTypes, null, ['id'=>'formDynamic', 'class'=> 'form-control input-lg'] ) !!}--}}
-                        </div>
-                    </div>
-                    <div class="col-sm-7">
-                        <div class="form-group" id="dynamicValue">
-                            <label>Data</label>
-                            <textarea class="form-control input-lg" type="value" name="value" required></textarea>
-                        </div>
-                    </div>
-                    <br>
-                    <div class="col-sm-1">
-                        <button style="margin: auto; display: block" type="submit" class="btn btn-orange mb-20">Add Dynamic</button>
-                    </div>
+            <div class="block block-bordered">
 
-                {{ Form::close() }}
-                <!-- END DYNAMIC DATA -->
+                <div class="block-content">
+                    <form class="form-horizontal push-10-t push-10" action="base_forms_premade.html" method="post" onsubmit="return false;">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <h3>Información</h3>
+                                <div class="form-group">
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="usuario">Usuario</label>
+                                        <input class="form-control input-lg" type="text" id="usuario" name="usuario" placeholder="Usuario">
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="email">Email</label>
+                                        <input class="form-control input-lg" type="email" id="email" name="email" placeholder="Email">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="password">Contraseña</label>
+                                        <input class="form-control input-lg" type="text" id="password" name="password" placeholder="Contraseña (Opcional)">
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label  for="role">Rol</label>
+                                        <select class="form-control input-lg" id="select-rol" name="select-rol" size="1">
+                                            <option value="0">- Seleccionar -</option>
+                                            <option value="1">Paciente</option>
+                                            <option value="2">Secretaria</option>
+                                            <option value="3">Doctor</option>
+                                            <option value="4">Administrador</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="dni">DNI</label>
+                                        <input class="form-control input-lg" type="text" id="dni" name="dni" placeholder="DNI">
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label  for="prepaid">Cobertura Médica</label>
+                                        <select class="form-control input-lg" id="select-prepaid" name="select-prepaid" size="1">
+                                            <option value="0">- Seleccionar -</option>
+                                            <option value="1">OSDE</option>
+                                            <option value="2">Medicus</option>
+                                            <option value="3">Swiss Medical</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <h3>Historia Clínica</h3>
+                                <div class="form-group">
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="medicacion">Medicación</label>
+                                        <textarea class="form-control input-lg" id="medicacion" name="medicacion" rows="5" placeholder="Medicación del Paciente..."></textarea>
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="enfermedades">Enfermedades</label>
+                                        <textarea class="form-control input-lg" id="enfermedades" name="enfermedades" rows="5" placeholder="Enfermedades del Paciente..."></textarea>
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="consultas">Consultas</label>
+                                        <input class="form-control input-lg" type="text" id="consultas" name="consultas" placeholder="Total de Consultas...">
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="operaciones">Operaciones</label>
+                                        <input class="form-control input-lg" type="text" id="operaciones" name="operaciones" placeholder="Total de Operaciones...">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                <button class="btn btn-success" type="submit"><i class="fa fa-check push-5-r"></i> Crear</button>
+                                <button class="btn btn-danger" type="submit"><i class="fa fa-remove push-5-r"></i> Cancelar</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
+        </div> <!-- End Block content -->
+    </div><!-- End Block bordered -->
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,153 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<!--[if IE 9]>         <html class="ie9 no-focus" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-focus" lang="en"> <!--<![endif]-->
+<base href="{{ asset('public/assets') }}">
+<head>
+    <meta charset="utf-8">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <title>Registrarse</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
-                        @csrf
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+    <link rel="shortcut icon" href="http://www.iconarchive.com/download/i82447/medicalwp/medical/Health-Sign-red.ico">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <link rel="icon" type="image/png" href="assets/img/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="assets/img/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="assets/img/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="assets/img/favicons/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="assets/img/favicons/favicon-192x192.png" sizes="192x192">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicons/apple-touch-icon-180x180.png">
+    <!-- END Icons -->
 
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+    <!-- Stylesheets -->
+    <!-- Web fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
+
+    <!-- Bootstrap and OneUI CSS framework -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" id="css-main" href="assets/css/oneui.css">
+
+    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
+    <!-- END Stylesheets -->
+</head>
+<body>
+<!-- Login Content -->
+<div class="bg-white pulldown">
+    <div class="content content-boxed overflow-hidden">
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                <div class="push-30-t push-50 animated fadeIn">
+                    <!-- Login Title -->
+                    <div class="text-center">
+                        <i class="fa fa-2x fa-plus-square text-primary"></i>
+                        <p class="text-muted push-15-t">CONSULTORIO | REGISTRARSE</p>
+                    </div>
+                    <!-- END Login Title -->
+
+                    <!-- Login Form -->
+                    <!-- jQuery Validation (.js-validation-login class is initialized in js/pages/base_pages_login.js) -->
+                    <!-- For more examples you can check out https://github.com/jzaefferer/jquery-validation -->
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                    {{--<form class="js-validation-login form-horizontal push-30-t" action="base_pages_dashboard.html" method="post">--}}
+                    <form class="js-validation-login form-horizontal push-30-t"" method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <div class="col-xs-12">
+                            <div class="form-material form-material-primary floating">
+                                <input id="login-username" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                {{--<input class="form-control" type="text" id="login-username" name="login-username">--}}
+                                <label for="login-username">Email</label>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                    </div>
+                    <div class="form-group">
+                        <div class="col-xs-6">
+                            <div class="form-material form-material-primary floating">
+                                <input id="login-username" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                {{--<input class="form-control" type="text" id="login-username" name="login-username">--}}
+                                <label for="login-username">Usuario</label>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="col-xs-6">
+                            <div class="form-material form-material-primary floating">
+                                <input id="login-password" type="text" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                {{--<input class="form-control" type="password" id="login-password" name="login-password">--}}
+                                <label for="login-password">DNI</label>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-xs-6">
+                            <div class="form-material form-material-primary floating">
+                                <input id="login-password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                {{--<input class="form-control" type="password" id="login-password" name="login-password">--}}
+                                <label for="login-password">Contraseña</label>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                        <div class="col-xs-6">
+                            <div class="form-material form-material-primary floating">
+                                <input id="login-password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                {{--<input class="form-control" type="password" id="login-password" name="login-password">--}}
+                                <label for="login-password">Confirmar Contraseña</label>
                             </div>
                         </div>
+                    </div>
+
+                    <div class="form-group push-30-t">
+                        <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+                            <button class="btn btn-sm btn-block btn-primary" type="submit">Registrarme</button>
+                        </div>
+                    </div>
                     </form>
+                    <!-- END Login Form -->
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+<!-- END Login Content -->
+
+<!-- Login Footer -->
+<div class="pulldown push-20-t text-center animated fadeInUp">
+    <small class="text-muted">DV &copy; 2018</small>
+</div>
+<!-- END Login Footer -->
+
+<!-- OneUI Core JS: jQuery, Bootstrap, slimScroll, scrollLock, Appear, CountTo, Placeholder, Cookie and App.js -->
+<script src="assets/js/core/jquery.min.js"></script>
+<script src="assets/js/core/bootstrap.min.js"></script>
+<script src="assets/js/core/jquery.slimscroll.min.js"></script>
+<script src="assets/js/core/jquery.scrollLock.min.js"></script>
+<script src="assets/js/core/jquery.appear.min.js"></script>
+<script src="assets/js/core/jquery.countTo.min.js"></script>
+<script src="assets/js/core/jquery.placeholder.min.js"></script>
+<script src="assets/js/core/js.cookie.min.js"></script>
+<script src="assets/js/app.js"></script>
+
+<!-- Page JS Plugins -->
+<script src="assets/js/plugins/jquery-validation/jquery.validate.min.js"></script>
+
+<!-- Page JS Code -->
+<script src="assets/js/pages/base_pages_login.js"></script>
+</body>
+</html>
