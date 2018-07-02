8aff9f3fe28c44364307737ec00dd43b9730bc05
FrancoPetrone <franco.petrone@davinci.edu.ar>
2018-07-01 23:41:53 -0300

    Merge branch 'master' of https://github.com/tom1mat/tpseguridad
    # Conflicts:
    #	resources/views/auth/login.blade.php
    #	resources/views/auth/register.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -3,7 +3,7 @@
 @section('main')
     <div class="block block-bordered">
         <div class="block-header bg-gray-lighter" style="border-bottom: 0">
-            <a href="{{ route("admin.index") }}"><span>USUARIOS</span></a>
+            <a href="{{ route("admin.index") }}"><span>USUARIOS > EDITAR</span></a>
         </div>
         <div class="block-content" style="padding: 0 35px">
             <div class="block block-bordered">
@@ -11,85 +11,80 @@
                 <div class="block-content">
                     <form class="form-horizontal push-10-t push-10" action="base_forms_premade.html" method="post" onsubmit="return false;">
                         <div class="row">
-                            <div class="col-sm-7">
+                            <div class="col-sm-12">
+                                <h3>Información</h3>
                                 <div class="form-group">
-                                    <div class="col-xs-6">
+                                    <div class="col-xs-12 col-lg-6">
                                         <label for="usuario">Usuario</label>
                                         <input class="form-control input-lg" type="text" id="usuario" name="usuario" placeholder="Usuario">
                                     </div>
-                                    <div class="col-xs-6">
+                                    <div class="col-xs-12 col-lg-6">
                                         <label for="email">Email</label>
-                                        <input class="form-control input-lg" type="text" id="email" name="email" placeholder="Email">
+                                        <input class="form-control input-lg" type="email" id="email" name="email" placeholder="Email">
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-sm-5">
+                            <div class="col-sm-12">
                                 <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <label for="dni">DNI</label>
-                                        <input class="form-control input-lg" type="text" id="dni" name="dni" placeholder="DNI">
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="password">Contraseña</label>
+                                        <input class="form-control input-lg" type="text" id="password" name="password" placeholder="Contraseña (Opcional)">
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-7">
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <label for="medicacion">Medicación</label>
-                                        <textarea class="form-control input-lg" id="medicacion" name="medicacion" rows="5" placeholder="Medicación del Paciente..."></textarea>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <label for="enfermedades">Enfermedades</label>
-                                        <textarea class="form-control input-lg" id="enfermedades" name="enfermedades" rows="5" placeholder="Enfermedades del Paciente..."></textarea>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-sm-5">
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <label for="mega-city">Where do you live?</label>
-                                        <input class="form-control input-lg" type="text" id="mega-city" name="mega-city" placeholder="Enter your location..">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-xs-12">
-                                        <label for="mega-skills">Skills</label>
-                                        <select class="form-control" id="mega-skills" name="mega-skills" size="7" multiple="">
-                                            <option value="1">HTML</option>
-                                            <option value="2">CSS</option>
-                                            <option value="3">JavaScript</option>
-                                            <option value="4">PHP</option>
-                                            <option value="5">Ruby</option>
-                                            <option value="6">Photoshop</option>
-                                            <option value="7">Illustrator</option>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label  for="role">Rol</label>
+                                        <select class="form-control input-lg" id="select-rol" name="select-rol" size="1">
+                                            <option value="0">- Seleccionar -</option>
+                                            <option value="1">Paciente</option>
+                                            <option value="2">Secretaria</option>
+                                            <option value="3">Doctor</option>
+                                            <option value="4">Administrador</option>
                                         </select>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="col-sm-12">
                                 <div class="form-group">
-                                    <div class="col-xs-6">
-                                        <label for="mega-age">Age</label>
-                                        <input class="form-control input-lg" type="text" id="mega-age" name="mega-age" placeholder="Enter your age..">
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="dni">DNI</label>
+                                        <input class="form-control input-lg" type="text" id="dni" name="dni" placeholder="DNI">
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label  for="prepaid">Cobertura Médica</label>
+                                        <select class="form-control input-lg" id="select-prepaid" name="select-prepaid" size="1">
+                                            <option value="0">- Seleccionar -</option>
+                                            <option value="1">OSDE</option>
+                                            <option value="2">Medicus</option>
+                                            <option value="3">Swiss Medical</option>
+                                        </select>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <h3>Historia Clínica</h3>
                                 <div class="form-group">
-                                    <label class="col-xs-12">Gender</label>
-                                    <div class="col-xs-12">
-                                        <label class="css-input css-radio css-radio-warning push-10-r">
-                                            <input type="radio" name="mega-gender-group"><span></span> Female
-                                        </label>
-                                        <label class="css-input css-radio css-radio-warning">
-                                            <input type="radio" name="mega-gender-group"><span></span> Male
-                                        </label>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="medicacion">Medicación</label>
+                                        <textarea class="form-control input-lg" id="medicacion" name="medicacion" rows="5" placeholder="Medicación del Paciente..."></textarea>
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="enfermedades">Enfermedades</label>
+                                        <textarea class="form-control input-lg" id="enfermedades" name="enfermedades" rows="5" placeholder="Enfermedades del Paciente..."></textarea>
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="consultas">Consultas</label>
+                                        <input class="form-control input-lg" type="text" id="consultas" name="consultas" placeholder="Total de Consultas...">
+                                    </div>
+                                    <div class="col-xs-12 col-lg-6">
+                                        <label for="operaciones">Operaciones</label>
+                                        <input class="form-control input-lg" type="text" id="operaciones" name="operaciones" placeholder="Total de Operaciones...">
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="form-group">
                             <div class="col-xs-12">
-                                <button class="btn btn-warning" type="submit"><i class="fa fa-check push-5-r"></i> Complete Profile</button>
+                                <button class="btn btn-success" type="submit"><i class="fa fa-check push-5-r"></i> Editar</button>
+                                <button class="btn btn-danger" type="submit"><i class="fa fa-remove push-5-r"></i> Cancelar</button>
                             </div>
                         </div>
                     </form>
